fed7a8e271c9d39b9de682af93f4bcf4a4c0319d
ZXZ <136934269@qq.com>
2016-12-21 18:03:20 +0800

    second commit

---
+++ Main/src/main/java/com/example/zxz/aidlservertest/SumService.java
@@ -0,0 +1,53 @@
+package com.example.zxz.aidlservertest;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Created by xuezhi.zxz on 2015/12/30.
+ */
+public class SumService extends Service{
+
+    private String TAG = "Test";
+    private SumImpl mBinder = new SumImpl();
+
+    private class SumImpl extends ISumAidlInterface.Stub {
+        @Override
+        public int getSum(int a, int b) throws RemoteException {
+            return a + b;
+        }
+
+        @Override
+        public void basicTypes(int anInt, long aLong, boolean aBoolean, float aFloat, double aDouble, String aString) throws RemoteException {
+
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        Log.d(TAG, "onCreate");
+        super.onCreate();
+    }
+
+    public IBinder onBind(Intent intent) {
+        Log.d(TAG, "onBind");
+        return mBinder;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.d(TAG, "onUnbind");
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "onDestroy");
+        super.onDestroy();
+    }
+}
